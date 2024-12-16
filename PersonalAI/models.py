7089cb0a42fc0c6152ffe0a456eafa12952720f7
syedadill <adilfazal03@gmail.com>
2024-12-16 15:09:36 +0500

    User

--- PersonalAI/models.py
+++ PersonalAI/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    #user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     first_name = models.CharField(max_length=255, null=True)
     last_name = models.CharField(max_length=255, null=True)
     date_of_birth = models.DateField(null=True)
